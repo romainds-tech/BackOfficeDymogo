abe30d994f1db02797c9b27b3efc3c0a916ec913
romain.da-silva@etu.univ-smb.fr <romain.da-silva@etu.univ-smb.fr>
2022-04-10 19:45:19 +0200

    architecture design

---
+++ src/components/Dashboard/ReportsCard/ReportCard/ReportCard.tsx
@@ -0,0 +1,18 @@
+//Import  css
+import './ReportCard.css';
+
+export function ReportCard() {
+    return (
+      <>
+        <div className='ReportCard'>
+            <div className='underblock'>
+                <div className='Username'>Gauthier Mauche</div>
+                <div className='date'></div>
+                <div className='time'></div>
+                <div className='location'></div>
+                <div className='status'></div>
+            </div>
+        </div>
+      </>
+    )
+  }
--- src/components/Dashboard/ReportsCard/ReportsCard.tsx
+++ src/components/Dashboard/ReportsCard/ReportsCard.tsx
@@ -1,10 +1,18 @@
 //Import  css
 import './ReportsCard.css';
+import { ReportCard } from './ReportCard/ReportCard';
 
 export function ReportsCard() {
     return (
       <>
         <div className='ReportsCard'>
+          <div className='bigTitle'>Reports</div>
+          <div className='filters'>Filters : </div>
+
+          <ReportCard />
+          <ReportCard />
+          <ReportCard />
+
         </div>
       </>
     )
