877df385497eaac2c8a6dcbb9f148722d8e7aa7a
NemesysS <38rom1@gmail.com>
2022-05-01 22:29:52 +0200

    wip design

--- src/components/Dashboard/ReportsCard/ReportsCard.tsx
+++ src/components/Dashboard/ReportsCard/ReportsCard.tsx
@@ -3,24 +3,126 @@ import './ReportsCard.css';
 import { ReportCard } from './ReportCard/ReportCard';
 
 //Import Badge
+import { Sorts } from '../../Sorts/Sorts';
+
+import {v4 as uuidv4} from 'uuid';
+
+import { useState } from 'preact/hooks';
 import { Filters } from '../../Filters/Filters';
 
-const itemlist = ["User", "Date", "Time", "Location", "Status"];
+const sortlist = ["User", "Date", "Time", "Location", "Status"];
+
+const filterslist = ["All", "Bad Park", "Waste", "Graffiti", "Sewer", "Other"];
 
 export function ReportsCard() {
+
+  //List of reports
+  const reports = [
+    {
+      id: uuidv4(),
+      username: "John Doe",
+      date: "12/12/2020",
+      time: "12:00",
+      address: "123 Main St",
+      status: "Pending",
+      type: "Bad Park"
+    },
+    {
+      id: uuidv4(),
+      username: "John Doe",
+      date: "12/12/2020",
+      time: "12:00",
+      address: "123 Main St",
+      status: "Pending",
+      type: "Sewer"
+    },
+    {
+      id: uuidv4(),
+      username: "John Doe",
+      date: "12/12/2020",
+      time: "12:00",
+      address: "123 Main St",
+      status: "Pending",
+      type: "Bad Park"
+    },
+    {
+      id: uuidv4(),
+      username: "John Doe",
+      date: "12/12/2020",
+      time: "12:00",
+      address: "123 Main St",
+      status: "Pending",
+      type: "Bad Park"
+    },
+    {
+      id: uuidv4(),
+      username: "John Doe",
+      date: "12/12/2020",
+      time: "12:00",
+      address: "123 Main St",
+      status: "Pending",
+      type: "Bad Park"
+    },
+    {
+      id: uuidv4(),
+      username: "John Doe",
+      date: "12/12/2020",
+      time: "12:00",
+      address: "123 Main St",
+      status: "Pending",
+      type: "Bad Park"
+    },
+    {
+      id: uuidv4(),
+      username: "John Doe",
+      date: "12/12/2020",
+      time: "12:00",
+      address: "123 Main St",
+      status: "Pending",
+      type: "Bad Park"
+    }
+    ,
+    {
+      id: uuidv4(),
+      username: "John Doe",
+      date: "12/12/2020",
+      time: "12:00",
+      address: "123 Main St",
+      status: "Pending",
+      type: "Bad Park"
+    }
+    ,
+    {
+      id: uuidv4(),
+      username: "John Doe",
+      date: "12/12/2020",
+      time: "12:00",
+      address: "123 Main St",
+      status: "Pending",
+      type: "Bad Park"
+    }
+  ]
+
+
+  
+
     return (
       <>
         <div className='ReportsCard'>
           <div className='bigTitle'>Reports</div>
+          
+          <Filters list={filterslist}></Filters>
 
-
-          <Filters list={itemlist} />
+          <Sorts list={sortlist} />
 
             
-
-          <ReportCard username='Gauthier Mauche #4325' date='12/03/2022' time='12:24:55' address='6 avenue de Genève 74000, Annecy' status='In progress' />
-          <ReportCard username='Gauthier Mauche #4325Gauthier Mauche #4325' date='12/03/2022' time='12:24:55' address='6 avenue de Genève 74000, Annecy' status='In progress' />
-          <ReportCard username='Gauthier Mauche #4325' date='12/03/2022' time='12:24:55' address='6 avenue de Genève 74000, Annecy' status='In progress' />
+          <div className='reports'>
+            
+            {reports.map(report => (
+              <ReportCard key={report.id} username={report.username} date={report.date} time={report.time} address={report.address} status={report.status} type={report.type} />
+            ))}
+          </div>
+          
 
         </div>
       </>
